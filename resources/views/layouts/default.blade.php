59b52f35c199714b13aad8e82547ebd8d62cc8f3
vagrant <vagrant@homestead>
2018-01-25 02:57:35 +0000

    静态页面加继承页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title','Sample')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link rel="stylesheet" href="code-guide.css">
+    <script src="code-guide.js"></script>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
